47db69a8d6944d94d377e9e60006f58500120fc0
Brandon McIntyre <mcintb7@mcmaster.ca>
2021-04-05 22:01:55 -0400

    monday 10pm work

---
+++ wingclean2.R
@@ -0,0 +1,109 @@
+library(tidyverse)
+library(ggplot2)
+
+#### Cleaning Data ####
+
+# put into rda file once sure is completely clean 
+
+raw_wing_table <- read_csv("NEW_CD_DGRP_Subset_Data_2019_V2.csv")
+
+str(raw_wing_table)
+
+wing_table_clean <- raw_wing_table %>% 
+  mutate(wing_size_mm = sqrt(TotalArea.px * 0.00215^2),
+         TotalArea.px = NULL,
+         Temperature = NULL, 
+         Allele_1 = factor(Allele_1, levels = c("OREw", "bx[1]", "bx[2]", 
+                                                "bx[3]","sd[29.1]", "sd[1]", 
+                                                "sd[E3]", "sd[ETX4]", "sd[58d]")), 
+         WT_Background = factor(WT_Background))
+
+levels(wing_table_clean$WT_Background)
+levels(wing_table_clean$Allele_1)
+
+source("scripts/ID_LeveneStat_V1_2016.R")
+
+#Create levene statistic
+lev_stat <- with(wing_table_clean, 
+                 LeveneDeviates(y = wing_size_mm, group = Allele_1:WT_Background, med = TRUE))
+
+wing_table_lev_raw <- wing_table_clean %>% mutate ( lev_stat = lev_stat)
+
+str(wing_table_clean)
+
+bxdat <- wing_table_clean %>% 
+  filter(Allele_1 %in% c("OREw", "bx[1]", "bx[2]", "bx[3]")) %>%
+  droplevels()
+
+str(bxdat)
+
+levels(bxdat$Allele_1)
+
+sddat<- wing_table_clean %>% 
+  filter(Allele_1 %in% c("OREw", "sd[1]", "sd[29.1]", "sd[58d]", "sd[E3]", "sd[ETX4]")) %>%
+  droplevels() 
+
+str(sddat)
+
+levels(sddat$Allele_1)
+
+
+#getting line means/sd/var and group sd/bx means/sd/var
+#maybe plot the variance over the means? 
+
+line_means_sd_var_cv <- wing_table_lev_raw %>% 
+  group_by(Allele_1, WT_Background) %>% 
+  summarise(length_means = mean(wing_size_mm), 
+            length_sd = sd(wing_size_mm),
+            length_cv = (sd(wing_size_mm)/mean(wing_size_mm))*100,
+            lev_stat = lev_stat,
+            Individuals = n()
+            )
+
+sd_means_sd_var_cv <- sddat %>% 
+  group_by(Allele_1, WT_Background) %>%
+  summarise(length_means = mean(wing_size_mm), 
+            length_sd = sd(wing_size_mm),
+            length_cv = (sd(wing_size_mm)/mean(wing_size_mm))*100,
+            lev_stat = lev_stat,
+            Individuals = n()
+  )
+
+bx_means_sd_var_cv <- bxdat %>%
+  group_by(Allele_1, WT_Background) %>%
+  summarise(length_means = mean(wing_size_mm), 
+            length_sd = sd(wing_size_mm),
+            length_cv = (sd(wing_size_mm)/mean(wing_size_mm))*100,
+            lev_stat = lev_stat,
+            Individuals = n()
+  )
+
+
+ggplot(line_means_sd_var_cv, aes(x=length_means, y=length_sd, color=Allele_1, size=Individuals)) +geom_point()
+  # kind of looks like a quadratic relationship? 
+
+ggplot(sd_means_sd_var_cv, aes(x=length_means, y=length_sd, color=Allele_1, size=Individuals)) + geom_point()
+# one maybe two outliers in the sd29.1 data set and wild type data set? 
+# maybe not log transformation is not applicable? looks like there may be a pattern there however,
+# would another transformation work better? quadratic? 
+
+ggplot(bx_means_sd_var_cv, aes(x=length_means, y=length_sd, colour=Allele_1, size=Individuals)) + geom_point()
+# this looks to have no observable pattern...
+
+ggplot(line_means_sd_var_cv, aes(x=length_means, y=length_sd, color=WT_Background)) + 
+  geom_point() + 
+  facet_wrap(~Allele_1)
+# maybe not log transform? 
+
+#display sample size for the dots 
+#bx log trasform would be better --> using levenes in raw form would be fine 
+
+line_meanssd <- wing_table_lev_raw %>% group_by(WT_Background) %>% summarise(line_means = mean(wing_size_mm),
+                                                                             line_sd = sd(wing_size_mm))
+
+ggplot(line_meanssd, aes(x=line_means, y=line_sd)) + geom_point()
+
+
+
+
+
