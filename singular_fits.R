5f1aeb49202996824d4c4fa3bfcc26e0fb2dd568
Ben Bolker <bbolker@gmail.com>
2021-04-09 16:28:55 -0400

    more comments on sing fits

--- singular_fits.R
+++ singular_fits.R
@@ -26,7 +26,20 @@ m2 <- update(m1, . ~ . - (1|Replicate) + Replicate)
 
 ## but, still singular.
 
-m3 <- update(m2, . ~ . - (0 + Allele_1 | WT_Background) + (1|WT_Background/Allele_1))
+## compound symmetry
+m3 <- update(m2, . ~ . - (0 + Allele_1 | WT_Background)
+             + (1|WT_Background/Allele_1))
+
+## lev_stat ~ Allele_1 + Replicate + (1 | WT_Background/Allele_1)
+## = ... + (1|WT_Background) + (1|WT_Background:Allele_1)
+
+## expected value of allele A within WT B =
+##   eps_{WT,B} + eps_{a:WT,A}
+## eps_WT ~ N(0,sigma^2_WT); eps_allele:WT ~ N(0, sigma^2_{a:WT}
+## for the covariance of i and j where i and j have the same WT & A
+## (variance) E[(eps_{WT,B} + eps_{a:WT,A})^2] = sigma^2_WT + sigma^2_{a:WT}
+## 
+
 VarCorr(m3)
 ## profile confidence intervals are fairly narrow
 confint(m3, parm="theta_", oldNames=FALSE)
@@ -49,6 +62,7 @@ ifun <- function(v,blank_diag=TRUE,...) {
                   xlab="",ylab="",sub="",...)
 }
 
+## correlation matrix for full model
 v2 <- cov2cor(VarCorr(m2)[[1]])
 
 ## construct compound symmetric matrix
@@ -56,9 +70,11 @@ v <- c(VarCorr(m3)[["Allele_1:WT_Background"]],
        VarCorr(m3)[["WT_Background"]])
 v3 <- matrix(v[2]/(v[1]+v[2]),nrow=nrow(v2),ncol=ncol(v2),
              dimnames=dimnames(v2))
+diag(v3) <- 1
 
 plot_grid(ifun(v2,main="unstructured"),ifun(v3, main="compound symm"))
 
+## principal components 
 rr <- rePCA(m2)
 plot(rr[[1]]$sdev,type="b",pch=16)
 
@@ -77,10 +93,12 @@ eigen(v2)$values
 eigen(v4)$values
 
 ## rank-reduced model: 5 components
-m5 <- glmmTMB(lev_stat ~ Allele_1 + rr(0 + Allele_1 | WT_Background,5) + Replicate,
-              data=wd, control=glmmTMBControl(optCtrl=list(iter.max=1000,eval.max=1000)))
+m5 <- glmmTMB(lev_stat ~ Allele_1 + rr(0 + Allele_1 | WT_Background,5) +
+                  Replicate,
+              data=wd,
+              control=glmmTMBControl(optCtrl=list(iter.max=1000,eval.max=1000)))
 v5 <- cov2cor(VarCorr(m5)$cond[[1]])
-plot_grid(ifun(v4,main="full"),ifun(v5, main="rr5"))
+plot_grid(ifun(v2,main="full"),ifun(v5, main="rr5"))
 zapsmall(eigen(v5)$values)
 AICtab(m1,m2,m3,m4,m5,
        mnames=c("full+rand rep","full","compsymm","TMB full+rep","rr5"),
